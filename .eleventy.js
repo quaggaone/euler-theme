f35b4bdabc63832910deddcebcfeef42fc400e09
Felix Dimmerling <ahoi@felixdim.com>
2024-08-26 16:05:10 +0200

    website release

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./assets/");
     
     eleventyConfig.addPassthroughCopy("./assets/.");
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" })
-
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
+    
     return {
         dir: {
             input: "src",
