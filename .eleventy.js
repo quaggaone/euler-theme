d68840fc6165f00c9074d60e4103436fc8421df4
Felix Dimmerling <ahoi@felixdim.com>
2024-12-29 16:51:28 +0100

    * moved assets into a modx based theme folder (`/assets/themes/...`)

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./assets/");
     
     eleventyConfig.addPassthroughCopy("./assets/.");
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/themes/hotbird/fonts/bootstrap-icons" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/themes/hotbird/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/themes/hotbird/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
     
     return {
         dir: {
