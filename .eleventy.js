e327e5939dbf240530691de82114c1fb5dce4f1b
Felix Dimmerling <ahoi@felixdim.com>
2025-03-18 16:20:34 +0100

    further euler renames

--- .eleventy.js
+++ .eleventy.js
@@ -6,9 +6,9 @@ module.exports = function (eleventyConfig) {
     });
     
     eleventyConfig.addPassthroughCopy("./assets/.");
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/themes/hotbird/fonts/bootstrap-icons" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/themes/hotbird/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/themes/hotbird/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/themes/euler/fonts/bootstrap-icons" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/themes/euler/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/themes/euler/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
     
     return {
         dir: {
