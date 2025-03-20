0355a6c5adbdae51a4d9d0829c4cc0a813068b25
Felix Dimmerling <ahoi@felixdim.com>
2025-03-20 02:26:01 +0100

    moved bootstrap js copy destination

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,8 @@ module.exports = function (eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("./assets/.");
     eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/themes/euler/fonts/bootstrap-icons" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/themes/euler/libs/bootstrap/dist/js/bootstrap.bundle.min.js" });
-    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/themes/euler/libs/bootstrap/dist/js/bootstrap.bundle.min.js.map" })
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/themes/euler/libs/bootstrap/js/bootstrap.bundle.min.js" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map": "assets/themes/euler/libs/bootstrap/js/bootstrap.bundle.min.js.map" })
     
     return {
         dir: {
